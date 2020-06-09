879757e0e7fc7c238ed81f86ee76de36670eee4a
eunbeen-ahn <aeb@sk.com>
2020-06-10 01:16:26 +0900

    feat: modify to use page template

--- src/pages/Todo/TodoPresenter.tsx
+++ src/pages/Todo/TodoPresenter.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import TodoForm from 'components/organisms/TodoForm';
 import TodoList from 'components/organisms/TodoList';
+import PageTemplate from 'components/templates/PageTemplate';
 
 function TodoPresenter() {
   return (
-    <>
+    <PageTemplate>
       <TodoForm />
       <TodoList />
-    </>
+    </PageTemplate>
   );
 }
 
